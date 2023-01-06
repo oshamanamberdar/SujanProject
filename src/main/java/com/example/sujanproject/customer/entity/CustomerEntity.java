8c6d52a37f2209f5d3d593300012aa83ba733f2a
oshama <info.oshama@gmail.com>
2023-01-06 15:44:31 +0530

    commit

--- src/main/java/com/example/sujanproject/customer/entity/CustomerEntity.java
+++ src/main/java/com/example/sujanproject/customer/entity/CustomerEntity.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 @AllArgsConstructor
@@ -20,11 +21,41 @@ public class CustomerEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String firstName;
+    private String customerName;
+    private Long contactNumber;
+    private Date orderDate;
+    private Date trialDate;
+    private Date deliveryDate;
+    private String coatLength;
+    private String coatChest;
+    private String coatWaist;
+    private String coatHip;
+    private String coatShoulder;
+    private String coatSleeveLength;
+    private String coatHalfBack;
+    private String coatNeck;
+    private String pantLength;
+    private String pantWaist;
+    private String pantHip;
+    private String pantThigh;
+    private String pantKnee;
+    private String pantBottom;
+    private String pantCrouch;
+    private String shirtLength;
+    private String shirtChest;
+    private String shirtWaist;
+    private String shirtHip;
+    private String shirtShoulder;
+    private String shirtSleeveLength;
+    private String shirtNeck;
+    private String note;
+    private String particular;
+    private Long totalAmount;
+    private Long advanceAmount;
+    private Long balanceAmount;
+    private String statusOfBalance;
+    private String statusOfProduct;
 
-    private String lastName;
 
-    private int phone;
 
-    private String address;
 }
